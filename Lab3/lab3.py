85e409acd590725879311647a439754859e19888
victorpapagelos <victorpapagelos@gmail.com>
2025-12-15 10:01:14 +0100

    sdf

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -11,16 +11,13 @@ y = data['class'].values
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2)
 
-#plt.scatter(x_train[y_train == 'Iris-virginica', 0], x_train[y_train == 'Iris-virginica', 1], color='tab:green', label='Iris-virginica')
-#plt.scatter(x_train[y_train == 'Iris-setosa', 0], x_train[y_train == 'Iris-setosa', 1], color='tab:red', label='Iris-setosa')
-#plt.scatter(x_train[y_train == 'Iris-versicolor', 0], x_train[y_train == 'Iris-versicolor', 1], color='tab:blue', label='Iris-versicolor')
-#plt.xlabel('sepal_length')
-#plt.ylabel('petal_width')
-#plt.legend()
-#plt.show
+plt.scatter(x_train[y_train == 'Iris-virginica', 0], x_train[y_train == 'Iris-virginica', 1], color='tab:green', label='Iris-virginica')
+plt.scatter(x_train[y_train == 'Iris-setosa', 0], x_train[y_train == 'Iris-setosa', 1], color='tab:red', label='Iris-setosa')
+plt.scatter(x_train[y_train == 'Iris-versicolor', 0], x_train[y_train == 'Iris-versicolor', 1], color='tab:blue', label='Iris-versicolor')
+plt.xlabel('sepal_length')
+plt.ylabel('petal_width')
+plt.legend()
+plt.show
 
 plt.plot([1,2,3], [4,5,6])
 plt.show()
-
-import os
-print(os.getcwd())
