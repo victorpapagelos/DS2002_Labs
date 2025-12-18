6d2eac7a610ab12ac6a316340de8ab8b8f51bef8
victorpapagelos <victorpapagelos@gmail.com>
2025-12-18 22:38:44 +0100

    dfgh

--- Lab3/task1_iterative_way_lab3.py
+++ Lab3/task1_iterative_way_lab3.py
@@ -1,73 +1,85 @@
 import csv
-import math
-from collections import Counter
-import matplotlib.pyplot as plt
 
-x = []
-y = []
-
-with open("iris_train.csv", "r") as file:
-    reader = csv.reader(file)
-    next(reader)  # skip header
-    for row in reader:
-        features = [float(value) for value in row[:-1]]
-        x.append(features)
-        y.append(row[-1])
-
-split_index = int(len(x) * 0.8)
-x_train = x[:split_index]
-y_train = y[:split_index]
-x_test = x[split_index:]
-y_test = y[split_index:]
-
-def euclidean_distance(a, b):
-    dist = 0
-    for i in range(len(a)):
-        dist += (a[i] - b[i]) ** 2
-    return math.sqrt(dist)
+def load(filename):
+    features = []
+    labels = []
+ 
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        next(reader)  # skip header
+        for row in reader:
+            features.append(row[:-1])
+            labels.append(row[-1])
+        
+        for i in range(len(features)):
+            for j in range(len(features[i])):
+                features[i][j] = float(features[i][j])
+    return (features, labels)
 
 class KNN:
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, x_train_data, y_train_data):
-        self.x_train = x_train_data
-        self.y_train = y_train_data
+    def euclidean_distance(self, A, B):
+        result = 0.0
+        for i in range(len(A)):
+            result += (A[i] - B[i]) ** 2
 
-    def predict(self, x_test_data):
+        return result ** 0.5
+
+    def fit(self, features_train, labels_train):
+        self.features_train = features_train
+        self.labels_train = labels_train
+
+    def predict_multiple(self, features_test):
         predictions = []
-        for i in range(len(x_test_data)):
-            point = x_test_data[i]
-            prediction = self.predict_point(point)
+        for element in features_test:
+            prediction = self.predict(element)
             predictions.append(prediction)
         return predictions
 
-    def predict_point(self, point):
+    def predict(self, new_element):
         distances = []
-        for i in range(len(self.x_train)):
-            train_point = self.x_train[i]
-            label = self.y_train[i]
-            dist = euclidean_distance(point, train_point)
-            distances.append((dist, label))
+        for i in range(len(self.features_train)):
+            element_feature = self.features_train[i]
+            element_label = self.labels_train[i]
 
-        distances.sort(key=lambda x: x[0])
+            distance = self.euclidean_distance(new_element, element_feature)
+            distances.append((distance, element_label))
+
+        distances.sort()
 
         k_nearest_labels = []
         for j in range(self.k):
             k_nearest_labels.append(distances[j][1])
 
-        counter = Counter(k_nearest_labels)
-        most_common = counter.most_common(1)[0][0]
-        return most_common
+        labels = {}
+        for label in k_nearest_labels:
+            if label in list(labels.keys()):
+                labels[label] += 1
+            else:
+                labels[label] = 1
 
-knn = KNN(k=3)
-knn.fit(x_train, y_train)
-predictions = knn.predict(x_test)
+        most_common_label = list(labels.keys())[0]
+        for label in labels:
+            if labels[label] > labels[most_common_label]:
+                most_common_label = label
+
+        return most_common_label
+
+
+
+features_train, labels_train = load("iris_train.csv")
+features_test, labels_test = load("iris_test.csv")
+
+knn = KNN(k=7)
+knn.fit(features_train, labels_train)
+predictions = knn.predict_multiple(features_test)
 
 correct = 0
-for i in range(len(y_test)):
-    if predictions[i] == y_test[i]:
+for i in range(len(labels_test)):
+    if predictions[i] == labels_test[i]:
         correct += 1
-accuracy = correct / len(y_test) * 100
+accuracy = correct / len(labels_test) * 100
 print(f"Accuracy: {accuracy:.2f}%")
 
