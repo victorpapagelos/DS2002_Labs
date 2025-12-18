0d5877ea0118484907382b65309e178c730d5b2c
victorpapagelos <victorpapagelos@gmail.com>
2025-12-18 23:49:45 +0100

    svdf

--- Lab3/task3_Kmeans_vectorized_lab3.py
+++ Lab3/task3_Kmeans_vectorized_lab3.py
@@ -28,11 +28,11 @@ class KMeans:
 
     def assign_clusters(self):
         cluster_distances = []
-        cluster = []
+        clusters = []
         
         #calculate the distance
         for i in range(self.k):
-            cluster.append([])
+            clusters.append([])
 
             cluster_center = self.cluster_centers[i]
             cluster_distances.append((np.sum((self.cluster_features - cluster_center) ** 2, axis=1)) ** 0.5) 
@@ -44,15 +44,44 @@ class KMeans:
                 if cluster_distances[closest][i] > cluster_distances[j][i]:
                     closest = j
 
+            clusters[closest].append(i)
+        return clusters
 
-                
+    def calculate_centers(self, clusters):
+        new_cluster_centers = []
+        for i in range(self.k):
+            features = np.zeros(shape=(len(clusters[i]), len(self.cluster_features[0])), dtype=float) 
+            # Y = cluster amount, X = feature amount
+            
+            for j in range(len(clusters[i])):
+                features[j] = self.cluster_features[clusters[i][j]]
 
+            # Calculate
+            new_cluster_centers.append(np.mean(features,axis=0))
 
+            pass
+        return np.array(new_cluster_centers)     
+
+    def run(self, steps):
+        for i in range(steps):
+            clusters = self.assign_clusters()
+            new_centers = self.calculate_centers(clusters)
+
+            if ((np.array(new_centers) == np.array(self.cluster_centers)).all()):
+                print(f'Finished after {i} steps.\nCenters: \n{new_centers}')
+                break
+            self.cluster_centers = new_centers
 
 
 
 cluster_features, cluster_labels = load("iris.csv")
 
+start = perf_counter()
+
 k = KMeans(2)
 k.fit(cluster_features)
+k.run(100)
 
+end = perf_counter()
+
+print(f"time: {end - start} seconds")
--- Lab3/task4_scikit_lab3.py
+++ Lab3/task4_scikit_lab3.py
@@ -3,26 +3,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cluster import KMeans as SKKMeans
 from sklearn.metrics import accuracy_score
+from time import perf_counter
 
 
 data = pd.read_csv("iris_train.csv")
-X = data.drop(columns=['class']).values#features
+x = data.drop(columns=['class']).values#features
 y = data['class'].values#labels
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+start = perf_counter()
 
 knn = KNeighborsClassifier(n_neighbors=3)
-knn.fit(X_train, y_train)
-knn_predictions = knn.predict(X_test)
+knn.fit(x_train, y_train)
+knn_predictions = knn.predict(x_test)
+
+end = perf_counter()
 
 knn_accuracy = accuracy_score(y_test, knn_predictions) * 100
 print(f"KNN Scikit-Learn Accuracy: {knn_accuracy:.2f}%")
 print("KNN Predictions (first 10 points):", knn_predictions[:10])
 
-X_all = X
+print(f"time: {end - start} seconds\n")
+
+x_all = x
+
+start = perf_counter()
 
 kmeans_skl = SKKMeans(n_clusters=3, random_state=2)
-kmeans_skl.fit(X_all)
+kmeans_skl.fit(x_all)
+
+end = perf_counter()
 
 cluster_labels = kmeans_skl.labels_
 centroids = kmeans_skl.cluster_centers_
@@ -30,3 +41,4 @@ centroids = kmeans_skl.cluster_centers_
 print("K-Means Cluster assignments (first 10 points):", cluster_labels[:10])
 print("K-Means Centroids:\n", centroids)
 
+print(f"time: {end - start} seconds")
